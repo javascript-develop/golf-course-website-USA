1486c62cad2c82cd9e0deb4407c830f338d32389
Rubel Hossain <rubelrana019914@gmail.com>
2023-09-08 12:34:40 +0600

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const sgMail = require('@sendgrid/mail');
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://michigansbestgolfdeals.com",
   })
 );
 app.use(cors());
