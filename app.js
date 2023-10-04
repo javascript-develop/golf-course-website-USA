bb04d209edcf608241743deca1ecc48466b7c0e8
Rubel Hossain <rubelrana019914@gmail.com>
2023-10-05 01:33:57 +0600

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const Comment = require('./modal/commentModal');
+const Comment = require('./modal/commentModal');
 require('dotenv').config();
 app.use(
   cors({
